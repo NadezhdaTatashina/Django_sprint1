f856837ef6fa8c5543d00f89a39d555a3fb7b7bd
Nadezhda Tatashina <tataschina73@yandex.ru>
2024-05-06 22:23:10 +0400

    испрены ошибки flake8

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def about(request):
-    """"Страница, отображающая информацию о проекте."""
+    """Страница, отображающая информацию о проекте."""
     template = 'pages/about.html'
     return render(request, template)
 
+
 def rules(request):
-    """"Страница, отображающая информацию о наших правилах."""
+    """Страница, отображающая информацию о наших правилах."""
     template = 'pages/rules.html'
     return render(request, template)
